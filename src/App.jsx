8110d7192923e01a893a675f07dd90ba0985f857
shunsaku-sugita <saku.dot@gmail.com>
2025-03-20 15:12:52 -0700

    終了：セクション４（React Router）

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { useState, useCallback, useMemo } from "react";
-import "./styles.css";
-import { ChildArea } from "./ChildArea";
+import { BrowserRouter, Link } from "react-router-dom/cjs/react-router-dom";
+import { Router } from "./router/Router";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-  const onChangeOpen = () => setOpen(!open);
-
-  // useCallbackは関数をメモ化するためのフック
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
-  // useMemoは値をメモ化するためのフック
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
-    <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onChangeOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Router />
+    </BrowserRouter>
   );
 }
