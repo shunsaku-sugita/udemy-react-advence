e2c793a91972f86835c66f943ef40411f1bae5ad
shunsaku-sugita <saku.dot@gmail.com>
2025-03-22 20:06:38 -0700

    終了：セクション８

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import "./styles.css";
-import { Router } from "./router/Router";
-import { UserProvider } from "./providers/UserProvider";
-import { RecoilRoot } from "recoil";
+// import "./styles.css";
+// import { Router } from "./router/Router";
+// import { UserProvider } from "./providers/UserProvider";
+// import { RecoilRoot } from "recoil";
+
+import axios from "axios";
 
 export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <RecoilRoot>
-      <UserProvider>
-        <Router />
-      </UserProvider>
-    </RecoilRoot>
+    // <RecoilRoot>
+    //   <UserProvider>
+    //     <Router />
+    //   </UserProvider>
+    // </RecoilRoot>
+
+    <div>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id-1のuser</button>
+    </div>
   );
 }
